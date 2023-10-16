ff2517ce8c982c3cc5cbc85e5685669896fc6d65
david.rodini <david.rodini@buonarroti.tn.it>
2023-10-16 08:46:50 +0200

    test: test metodo clone

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona = new Persona("Frank", 2000, "M");
-        System.out.println(persona.toString());
+        Persona persona1 = new Persona("Frank", 2000, "M");
+        System.out.println(persona1.toString());
+        Persona persona2 = (Persona) persona1.clone();
+        persona2.setNome("Paolo");
+        System.out.println(persona2.clone());
     }
 }
