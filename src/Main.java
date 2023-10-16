823b94b27590d6e0993c2c63832b1c3c879d16ea
david.rodini <david.rodini@buonarroti.tn.it>
2023-10-16 08:29:33 +0200

    test: stampa persona

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Persona persona = new Persona("Frank", 2000, "M");
+        System.out.println(persona.toString());
     }
 }
