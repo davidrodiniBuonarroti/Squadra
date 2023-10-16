f993c344b6f6d2526e22c95963c8f6a4b0cd55b6
david.rodini <david.rodini@buonarroti.tn.it>
2023-10-16 09:53:46 +0200

    test: clone calciatore

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona("Frank", 2000, "M");
+        Persona persona1 = new Persona("Frank", 2000, "M", 43);
         System.out.println(persona1.toString());
         Persona persona2 = (Persona) persona1.clone();
         persona2.setNome("Paolo");
         System.out.println(persona2.clone());
 
-        Calciatore calciatore1 = new Calciatore("Cristiano", 1985, "M", "Punta", 7, 1000);
+        Calciatore calciatore1 = new Calciatore("Cristiano", 1985, "M", 44, "Punta", 7, 1000);
         System.out.println(calciatore1.toString());
 
         Calciatore calciatore2 = (Calciatore) calciatore1.clone();
