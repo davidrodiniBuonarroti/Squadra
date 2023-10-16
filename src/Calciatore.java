9366a7c8e4cce93dd732571c843cbb0e3c3e8c87
david.rodini <david.rodini@buonarroti.tn.it>
2023-10-16 08:51:57 +0200

    feat: costruttore

--- src/Calciatore.java
+++ src/Calciatore.java
@@ -1,2 +1,14 @@
 public class Calciatore extends Persona{
+
+    private String ruolo;
+    private int nMaglia;
+    private int nGoal;
+
+    public Calciatore(String nome, int annoNascita, String sesso,
+                      String ruolo, int nMaglia, int nGoal){
+        super(nome, annoNascita, sesso);
+        this.ruolo = ruolo;
+        this.nMaglia = nMaglia;
+        this.nGoal = nGoal;
+    }
 }
