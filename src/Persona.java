553b6a1dace4dd94b8bde5edc8d0845fc27b62aa
david.rodini <david.rodini@buonarroti.tn.it>
2023-10-16 08:23:48 +0200

    feat: metodi base

--- src/Persona.java
+++ src/Persona.java
@@ -1,2 +1,19 @@
 public class Persona {
+
+    private String nome;
+    private int annoNascita;
+    private String sesso;
+
+    public Persona(String nome, int annoNascita, String sesso){
+        this.nome = nome;
+        this.annoNascita = annoNascita;
+        this.sesso = sesso;
+    }
+
+    public String toString(){
+        String rit = "{Nome: " + this.nome + ",\n";
+        rit += "Anno di nascità: " + this.annoNascita + ",\n";
+        rit += "Sesso: " + this.sesso + "}\n";
+        return rit;
+    }
 }
